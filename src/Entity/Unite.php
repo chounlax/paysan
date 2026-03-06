327a25823f79cf01e28699667f8f0886deea81a9
mouad <mouad.hayani05@gmail.com>
2026-03-06 11:09:45 +0100

    Ajout de la table

--- src/Entity/Unite.php
+++ src/Entity/Unite.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UniteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UniteRepository::class)]
@@ -15,6 +17,24 @@ class Unite
 
     #[ORM\Column(nullable: true)]
     private ?float $unite = null;
+
+    /**
+     * @var Collection<int, Elementschimiques>
+     */
+    #[ORM\OneToMany(targetEntity: Elementschimiques::class, mappedBy: 'unite')]
+    private Collection $elementschimiques;
+
+    /**
+     * @var Collection<int, Elementschimiques>
+     */
+    #[ORM\OneToMany(targetEntity: Elementschimiques::class, mappedBy: 'unite', orphanRemoval: true)]
+    private Collection $elementchimique;
+
+    public function __construct()
+    {
+        $this->elementschimiques = new ArrayCollection();
+        $this->elementchimique = new ArrayCollection();
+    }
 
 
     public function getId(): ?int
@@ -30,6 +50,66 @@ class Unite
     public function setUnite(?float $unite): static
     {
         $this->unite = $unite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Elementschimiques>
+     */
+    public function getElementschimiques(): Collection
+    {
+        return $this->elementschimiques;
+    }
+
+    public function addElementschimique(Elementschimiques $elementschimique): static
+    {
+        if (!$this->elementschimiques->contains($elementschimique)) {
+            $this->elementschimiques->add($elementschimique);
+            $elementschimique->setUnite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeElementschimique(Elementschimiques $elementschimique): static
+    {
+        if ($this->elementschimiques->removeElement($elementschimique)) {
+            // set the owning side to null (unless already changed)
+            if ($elementschimique->getUnite() === $this) {
+                $elementschimique->setUnite(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Elementschimiques>
+     */
+    public function getElementchimique(): Collection
+    {
+        return $this->elementchimique;
+    }
+
+    public function addElementchimique(Elementschimiques $elementchimique): static
+    {
+        if (!$this->elementchimique->contains($elementchimique)) {
+            $this->elementchimique->add($elementchimique);
+            $elementchimique->setUnite($this);
+        }
+
+        return $this;
+    }
+
+    public function removeElementchimique(Elementschimiques $elementchimique): static
+    {
+        if ($this->elementchimique->removeElement($elementchimique)) {
+            // set the owning side to null (unless already changed)
+            if ($elementchimique->getUnite() === $this) {
+                $elementchimique->setUnite(null);
+            }
+        }
 
         return $this;
     }
