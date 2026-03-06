932f62030c34e5a848e742792b0f39eb927ffbe4
Zlatan <sd.thompson80200@gmail.com>
2026-03-06 09:53:00 +0100

    Initialisation du Projet

---
+++ src/Controller/PaysanController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PaysanController extends AbstractController
+{
+    #[Route('/paysan', name: 'app_paysan')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('base/contact.html.twig');
+    }
+}
